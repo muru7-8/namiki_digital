3f69b266877197a631afd8963f1cac1edef8f697
Nic <nicmottas@gmail.com>
2021-01-09 11:31:24 -0300

    test

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ client.onMessageArrived = onMessageArrived;
 */
 
 let x, y;
+let mensajeIN;
 
 
 
@@ -19,8 +20,8 @@ function setup(){
 /*
     client.connect({
 		onSuccess: onConnect,
-		userName: "connected-object",
-		password: "c784e41dd3da48d4",
+		userName: "b9b9d1f7",
+		password: "860f838a013c6f86",
 		useSSL: true,
     });
 */
@@ -34,13 +35,43 @@ function draw(){
     fill(255);
     textSize(35);
     text("orientacion: " + deviceOrientation, 100, 100);
-    text("ace X: " + accelerationX, 100, 120);
-    text("ace Y: " + accelerationY, 100, 140);
-    text("ace Z: " + accelerationZ, 100, 160);
-    text("rot X: " + rotationX, 100, 180);
-    text("rot Y: " + rotationY, 100, 200);
-    text("rot Z: " + rotationZ, 100, 220);
+    text("ace X: " + accelerationX, 100, 130);
+    text("ace Y: " + accelerationY, 100, 160);
+    text("ace Z: " + accelerationZ, 100, 190);
+    text("rot X: " + rotationX, 100, 220);
+    text("rot Y: " + rotationY, 100, 250);
+    text("rot Z: " + rotationZ, 100, 280);
 }
+
+/*
+function onConnect() {
+	// Once a connection has been made, make a subscription and send a message.
+    console.log("Conectado a server MQTT");
+    // subscribe
+	client.subscribe("/namiki/mobile/rotacionX");
+}
+
+function onConnectionLost(responseObject) {
+	if (responseObject.errorCode !== 0) {
+		console.log("onConnectionLost:" + responseObject.errorMessage);
+	}
+}
+
+function onMessageArrived(message) {
+	console.log(message.destinationName + " -> " + message.payloadString);
+
+    var payload = message.payloadString;
+    console.log("rotacion X: " + payload);
+
+}
+
+
+function sendMessage(topic, message) {
+	message = new Paho.MQTT.Message(message);
+	message.destinationName = topic;
+	client.send("HOLA");
+}
+*/
 
 
 var client = mqtt.connect('mqtt://b9b9d1f7:860f838a013c6f86@broker.shiftr.io', {
@@ -50,14 +81,27 @@ var client = mqtt.connect('mqtt://b9b9d1f7:860f838a013c6f86@broker.shiftr.io', {
 client.on('connect', function(){
   console.log('client has connected!');
 
-  client.subscribe('/example');
+  client.subscribe('/hello');
+  client.subscribe('/hello_1');
   // client.unsubscribe('/example');
 
   setInterval(function(){
-    client.publish('/hello', rotationX);
+    client.publish('/hello', String(rotationX));
+    client.publish('/hello_1', String(random(0, 100)));
   }, 1000);
+  
 });
 
 client.on('message', function(topic, message) {
-  console.log('new message:', topic, message.toString());
+  //console.log('new message:', topic, message.toString());
+  mensajeIN = message.toString();
+
+  if (topic == "/hello_1"){
+    console.log("numero Random: " + mensajeIN);
+  }
+
+  if (topic == "/hello"){
+    console.log("rotacion X: " + mensajeIN);
+  }
+  
 });
