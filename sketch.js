a9b86f691965c408362816eec7be5b21733ed49e
Nic <nicmottas@gmail.com>
2021-01-09 10:41:05 -0300

    aceleracion y rotacion mobile

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
+
+let x, y;
+
+
 function setup(){
-    createCanvas(400,400);
+    x = windowWidth;
+    y = windowHeight;
+
+    createCanvas(x - 20, y - 20);
 }
 
 function draw(){
     background(0);
-    ellipse(200, 200, 200);
+    ellipse(x / 2, y / 2, 200);
+
+    fill(255);
+    textSize(20);
+    text("orientacion: " + deviceOrientation, 100, 100);
+    text("ace X: " + accelerationX, 100, 120);
+    text("ace Y: " + accelerationY, 100, 140);
+    text("ace Z: " + accelerationZ, 100, 160);
+    text("rot X: " + rotationX, 100, 180);
+    text("rot Y: " + rotationY, 100, 200);
+    text("rot Z: " + rotationZ, 100, 220);
 }
