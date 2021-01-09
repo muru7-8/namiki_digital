d83ee3080c2f47a177d6dd0af3173c9ae1ffc0f1
Nic <nicmottas@gmail.com>
2021-01-09 14:09:46 -0300

    test online

--- sketch.js
+++ sketch.js
@@ -1,107 +1,118 @@
 
+ 
 /*
-//setup MQTT client
-client = new Paho.MQTT.Client("broker.shiftr.io", Number(443), "controlpanel");
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+    p5.js MQTT Client example
+    This example uses p5.js: https://p5js.org/
+    and the Eclipse Paho MQTT client library: https://www.eclipse.org/paho/clients/js/
+    to create an MQTT client that sends and receives MQTT messages.
+    The client is set up for use on the shiftr.io test MQTT broker (https://next.shiftr.io/try),
+    but has also been tested on https://test.mosquitto.org
+    created 12 June 2020
+    modified 20 Aug 2020
+    by Tom Igoe
 */
 
-let x, y;
-let mensajeIN;
+// MQTT client details:
+let broker = {
+    hostname: 'broker.shiftr.io',
+    port: 443
+};
+// MQTT client:
+let client;
+// client credentials:
+let creds = {
+    clientID: 'p5ClientNEC',
+    userName: 'b9b9d1f7',
+    password: '860f838a013c6f86'
+}
+// topic to subscribe to when you connect:
+let topic = 'notes';
 
+// a pushbutton to send messages
+let sendButton;
+let localDiv;
+let remoteDiv;
 
+// intensity of the circle in the middle
+let intensity = 255;
 
-function setup(){
-    x = windowWidth;
-    y = windowHeight;
-
-    createCanvas(x - 20, y - 20);
-
-/*
-    client.connect({
-		onSuccess: onConnect,
-		userName: "b9b9d1f7",
-		password: "860f838a013c6f86",
-		useSSL: true,
-    });
-*/
-
+function setup() {
+    createCanvas(400, 400);
+    // Create an MQTT client:
+    client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
+    // set callback handlers for the client:
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    // connect to the MQTT broker:
+    client.connect(
+        {
+            onSuccess: onConnect,       // callback function for when you connect
+            userName: creds.userName,   // username
+            password: creds.password,   // password
+            useSSL: true                // use SSL
+        }
+    );
+    // create the send button:
+    sendButton = createButton('send a message');
+    sendButton.position(20, 20);
+    sendButton.mousePressed(sendMqttMessage);
+    // create a div for local messages:
+    localDiv = createDiv('local messages will go here');
+    localDiv.position(20, 50);
+    localDiv.style('color', '#fff');
+    // create a div for the response:
+    remoteDiv = createDiv('waiting for messages');
+    remoteDiv.position(20, 80);
+    remoteDiv.style('color', '#fff');
 }
 
-function draw(){
-    background(0);
-    ellipse(x / 2, y / 2, 200);
-
-    fill(255);
-    textSize(35);
-    text("orientacion: " + deviceOrientation, 100, 100);
-    text("ace X: " + accelerationX, 100, 130);
-    text("ace Y: " + accelerationY, 100, 160);
-    text("ace Z: " + accelerationZ, 100, 190);
-    text("rot X: " + rotationX, 100, 220);
-    text("rot Y: " + rotationY, 100, 250);
-    text("rot Z: " + rotationZ, 100, 280);
+function draw() {
+    background(50);
+    // draw a circle whose brightness changes when a message is received:
+    fill(intensity);
+    circle(width/2, height/2, width/2);
+// subtract one from the brightness of the circle:
+    if (intensity > 0) {
+        intensity--;
+    }
 }
 
-/*
+// called when the client connects
 function onConnect() {
-	// Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado a server MQTT");
-    // subscribe
-	client.subscribe("/namiki/mobile/rotacionX");
+    localDiv.html('client is connected');
+    client.subscribe(topic);
 }
 
-function onConnectionLost(responseObject) {
-	if (responseObject.errorCode !== 0) {
-		console.log("onConnectionLost:" + responseObject.errorMessage);
-	}
+// called when the client loses its connection
+function onConnectionLost(response) {
+    if (response.errorCode !== 0) {
+        localDiv.html('onConnectionLost:' + response.errorMessage);
+    }
 }
 
+// called when a message arrives
 function onMessageArrived(message) {
-	console.log(message.destinationName + " -> " + message.payloadString);
-
-    var payload = message.payloadString;
-    console.log("rotacion X: " + payload);
-
+    remoteDiv.html('I got a message:' + message.payloadString);
+    let  incomingNumber = parseInt(message.payloadString);
+    if (incomingNumber > 0) {
+        intensity = 255;
+    }
 }
 
-
-function sendMessage(topic, message) {
-	message = new Paho.MQTT.Message(message);
-	message.destinationName = topic;
-	client.send("HOLA");
+// called when you want to send a message:
+function sendMqttMessage() {
+    // if the client is connected to the MQTT broker:
+    if (client.isConnected()) {
+        // make a string with a random number form 0 to 15:
+        let msg = String(round(random(15)));
+        // start an MQTT message:
+        message = new Paho.MQTT.Message(msg);
+        // choose the destination topic:
+        message.destinationName = topic;
+        // send it:
+        client.send(message);
+        // print what you sent:
+        localDiv.html('I sent: ' + message.payloadString);
+    }
 }
-*/
 
-
-var client = mqtt.connect('mqtt://b9b9d1f7:860f838a013c6f86@broker.shiftr.io', 443, {
-  clientId: 'javascriptNEC'
-});
-
-client.on('connect', function(){
-  console.log('client has connected!');
-
-  client.subscribe('/hello');
-  client.subscribe('/hello_1');
-  // client.unsubscribe('/example');
-
-  setInterval(function(){
-    client.publish('/hello', String(rotationX));
-    client.publish('/hello_1', String(random(0, 100)));
-  }, 1000);
-  
-});
-
-client.on('message', function(topic, message) {
-  //console.log('new message:', topic, message.toString());
-  mensajeIN = message.toString();
-
-  if (topic == "/hello_1"){
-    console.log("numero Random: " + mensajeIN);
-  }
-
-  if (topic == "/hello"){
-    console.log("rotacion X: " + mensajeIN);
-  }
-  
-});
---
+++ sketch_old.js
@@ -0,0 +1,107 @@
+
+/*
+//setup MQTT client
+client = new Paho.MQTT.Client("broker.shiftr.io", Number(443), "controlpanel");
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+*/
+
+let x, y;
+let mensajeIN;
+
+
+
+function setup(){
+    x = windowWidth;
+    y = windowHeight;
+
+    createCanvas(x - 20, y - 20);
+
+/*
+    client.connect({
+		onSuccess: onConnect,
+		userName: "b9b9d1f7",
+		password: "860f838a013c6f86",
+		useSSL: true,
+    });
+*/
+
+}
+
+function draw(){
+    background(0);
+    ellipse(x / 2, y / 2, 200);
+
+    fill(255);
+    textSize(35);
+    text("orientacion: " + deviceOrientation, 100, 100);
+    text("ace X: " + accelerationX, 100, 130);
+    text("ace Y: " + accelerationY, 100, 160);
+    text("ace Z: " + accelerationZ, 100, 190);
+    text("rot X: " + rotationX, 100, 220);
+    text("rot Y: " + rotationY, 100, 250);
+    text("rot Z: " + rotationZ, 100, 280);
+}
+
+/*
+function onConnect() {
+	// Once a connection has been made, make a subscription and send a message.
+    console.log("Conectado a server MQTT");
+    // subscribe
+	client.subscribe("/namiki/mobile/rotacionX");
+}
+
+function onConnectionLost(responseObject) {
+	if (responseObject.errorCode !== 0) {
+		console.log("onConnectionLost:" + responseObject.errorMessage);
+	}
+}
+
+function onMessageArrived(message) {
+	console.log(message.destinationName + " -> " + message.payloadString);
+
+    var payload = message.payloadString;
+    console.log("rotacion X: " + payload);
+
+}
+
+
+function sendMessage(topic, message) {
+	message = new Paho.MQTT.Message(message);
+	message.destinationName = topic;
+	client.send("HOLA");
+}
+*/
+
+
+var client = mqtt.connect('mqtt://b9b9d1f7:860f838a013c6f86@broker.shiftr.io', 443, {
+  clientId: 'javascriptNEC'
+});
+
+client.on('connect', function(){
+  console.log('client has connected!');
+
+  client.subscribe('/hello');
+  client.subscribe('/hello_1');
+  // client.unsubscribe('/example');
+
+  setInterval(function(){
+    client.publish('/hello', String(rotationX));
+    client.publish('/hello_1', String(random(0, 100)));
+  }, 1000);
+  
+});
+
+client.on('message', function(topic, message) {
+  //console.log('new message:', topic, message.toString());
+  mensajeIN = message.toString();
+
+  if (topic == "/hello_1"){
+    console.log("numero Random: " + mensajeIN);
+  }
+
+  if (topic == "/hello"){
+    console.log("rotacion X: " + mensajeIN);
+  }
+  
+});
