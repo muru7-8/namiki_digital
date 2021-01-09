d740bbe3eea1258f3e7badd5777722fd657a90d5
Nic <nicmottas@gmail.com>
2021-01-09 15:17:36 -0300

    test_1

--- sketch.js
+++ sketch.js
@@ -1,44 +1,31 @@
 
- 
-/*
-    p5.js MQTT Client example
-    This example uses p5.js: https://p5js.org/
-    and the Eclipse Paho MQTT client library: https://www.eclipse.org/paho/clients/js/
-    to create an MQTT client that sends and receives MQTT messages.
-    The client is set up for use on the shiftr.io test MQTT broker (https://next.shiftr.io/try),
-    but has also been tested on https://test.mosquitto.org
-    created 12 June 2020
-    modified 20 Aug 2020
-    by Tom Igoe
-*/
-
 // MQTT client details:
 let broker = {
-    hostname: 'broker.shiftr.io',
-    port: 443
+  hostname: 'broker.shiftr.io',
+  port: 443
 };
 // MQTT client:
 let client;
 // client credentials:
 let creds = {
-    clientID: 'p5ClientNEC',
-    userName: 'b9b9d1f7',
-    password: '860f838a013c6f86'
+  clientID: 'p5ClientNEC',
+  userName: 'b9b9d1f7',
+  password: '860f838a013c6f86'
 }
-// topic to subscribe to when you connect:
-let topic = 'notes';
 
-// a pushbutton to send messages
-let sendButton;
-let localDiv;
-let remoteDiv;
 
-// intensity of the circle in the middle
-let intensity = 255;
+let x, y;
+let mensajeIN;
 
-function setup() {
-    createCanvas(400, 400);
-    // Create an MQTT client:
+
+
+function setup(){
+    x = windowWidth;
+    y = windowHeight;
+
+    createCanvas(x - 20, y - 20);
+
+
     client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
     // set callback handlers for the client:
     client.onConnectionLost = onConnectionLost;
@@ -52,67 +39,78 @@ function setup() {
             useSSL: true                // use SSL
         }
     );
-    // create the send button:
-    sendButton = createButton('send a message');
-    sendButton.position(20, 20);
-    sendButton.mousePressed(sendMqttMessage);
-    // create a div for local messages:
-    localDiv = createDiv('local messages will go here');
-    localDiv.position(20, 50);
-    localDiv.style('color', '#fff');
-    // create a div for the response:
-    remoteDiv = createDiv('waiting for messages');
-    remoteDiv.position(20, 80);
-    remoteDiv.style('color', '#fff');
+
+    setInterval(sendMqttMessage, 500);
+
 }
 
-function draw() {
-    background(50);
-    // draw a circle whose brightness changes when a message is received:
-    fill(intensity);
-    circle(width/2, height/2, width/2);
-// subtract one from the brightness of the circle:
-    if (intensity > 0) {
-        intensity--;
-    }
+function draw(){
+    background(0);
+    ellipse(x / 2, y / 2, 200);
+
+    fill(255);
+    textSize(35);
+    text("orientacion: " + deviceOrientation, 100, 100);
+    text("ace X: " + accelerationX, 100, 130);
+    text("ace Y: " + accelerationY, 100, 160);
+    text("ace Z: " + accelerationZ, 100, 190);
+    text("rot X: " + rotationX, 100, 220);
+    text("rot Y: " + rotationY, 100, 250);
+    text("rot Z: " + rotationZ, 100, 280);
+
+    
+    
 }
 
 // called when the client connects
 function onConnect() {
-    localDiv.html('client is connected');
-    client.subscribe(topic);
+  console.log('client is connected');
+  client.subscribe("/namiki/rotacionX");
+  client.subscribe("/namiki/rotacionY");
+  client.subscribe("/namiki/rotacionZ");
+
 }
 
 // called when the client loses its connection
 function onConnectionLost(response) {
-    if (response.errorCode !== 0) {
-        localDiv.html('onConnectionLost:' + response.errorMessage);
-    }
+  if (response.errorCode !== 0) {
+      console.log('onConnectionLost:' + response.errorMessage);
+  }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-    remoteDiv.html('I got a message:' + message.payloadString);
-    let  incomingNumber = parseInt(message.payloadString);
-    if (incomingNumber > 0) {
-        intensity = 255;
-    }
+  let  incomingNumber = parseInt(message.payloadString);
 }
 
 // called when you want to send a message:
 function sendMqttMessage() {
-    // if the client is connected to the MQTT broker:
-    if (client.isConnected()) {
-        // make a string with a random number form 0 to 15:
-        let msg = String(round(random(15)));
-        // start an MQTT message:
-        message = new Paho.MQTT.Message(msg);
-        // choose the destination topic:
-        message.destinationName = topic;
-        // send it:
-        client.send(message);
-        // print what you sent:
-        localDiv.html('I sent: ' + message.payloadString);
-    }
-}
+  // if the client is connected to the MQTT broker:
+  if (client.isConnected()) {
+      rotacionX = new Paho.MQTT.Message(String(rotationX));
+      rotacionX.destinationName = "namiki/rotacionX";
+      client.send(rotacionX);
 
+      rotacionY = new Paho.MQTT.Message(String(rotationY));
+      rotacionY.destinationName = "namiki/rotacionY";
+      client.send(rotacionY);
+
+      rotacionZ = new Paho.MQTT.Message(String(rotationZ));
+      rotacionZ.destinationName = "namiki/rotacionZ";
+      client.send(rotacionZ);
+
+      aceX = new Paho.MQTT.Message(String(accelerationX));
+      aceX.destinationName = "namiki/aceX";
+      client.send(aceX);
+
+      aceY = new Paho.MQTT.Message(String(accelerationY));
+      aceY.destinationName = "namiki/aceY";
+      client.send(aceY);
+
+      aceZ = new Paho.MQTT.Message(String(accelerationZ));
+      aceZ.destinationName = "namiki/aceZ";
+      client.send(aceZ);
+
+      
+  }
+}
