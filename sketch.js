f59338475971746a5bdb2a1523887162cdd7d263
Nic <nicmottas@gmail.com>
2021-01-09 11:06:40 -0300

    mqtt test

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 
+/*
+//setup MQTT client
+client = new Paho.MQTT.Client("broker.shiftr.io", Number(443), "controlpanel");
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+*/
+
 let x, y;
+
 
 
 function setup(){
@@ -7,6 +15,16 @@ function setup(){
     y = windowHeight;
 
     createCanvas(x - 20, y - 20);
+
+/*
+    client.connect({
+		onSuccess: onConnect,
+		userName: "connected-object",
+		password: "c784e41dd3da48d4",
+		useSSL: true,
+    });
+*/
+
 }
 
 function draw(){
@@ -14,7 +32,7 @@ function draw(){
     ellipse(x / 2, y / 2, 200);
 
     fill(255);
-    textSize(20);
+    textSize(35);
     text("orientacion: " + deviceOrientation, 100, 100);
     text("ace X: " + accelerationX, 100, 120);
     text("ace Y: " + accelerationY, 100, 140);
@@ -23,3 +41,23 @@ function draw(){
     text("rot Y: " + rotationY, 100, 200);
     text("rot Z: " + rotationZ, 100, 220);
 }
+
+
+var client = mqtt.connect('mqtt://b9b9d1f7:860f838a013c6f86@broker.shiftr.io', {
+  clientId: 'javascriptNEC'
+});
+
+client.on('connect', function(){
+  console.log('client has connected!');
+
+  client.subscribe('/example');
+  // client.unsubscribe('/example');
+
+  setInterval(function(){
+    client.publish('/hello', rotationX);
+  }, 1000);
+});
+
+client.on('message', function(topic, message) {
+  console.log('new message:', topic, message.toString());
+});
