7a17e8db1dd348e254a6ef11b7cdc476d5df5b99
NicMotta <34142261+NicMotta@users.noreply.github.com>
2021-01-28 19:30:56 -0300

    asda

--- sketch.js
+++ sketch.js
@@ -193,7 +193,7 @@ function draw(){
 
 
     lights();
-    ambientLight(0);
+    ambientLight(20);
 
     push();
     texture(skySphere);
@@ -257,7 +257,7 @@ function draw(){
     //fill(0, 250, 100);
     texture(texturaPiedra1);
     noStroke();
-    pointLight(0,255,0, 0, 0, 0);
+    //pointLight(255,255,255, 0, 0, 0);
     for (let i = 0; i < contador; i++){
       push();
       rotateY(i+i*2);
@@ -278,7 +278,7 @@ function draw(){
     //fill(0, 100, 250);
     texture(texturaPiedra2);
     noStroke();
-    pointLight(0,255,0, 0, 0, 0);
+    //pointLight(255,255,255, 0, 0, 0);
     for (let i = 0; i < contador; i++){
       push();
       rotateY(i+i*2);
@@ -297,7 +297,7 @@ function draw(){
     //fill(250, 300, 0);
     texture(texturaPiedra3);
     noStroke();
-    pointLight(0,255,0, 0, 0, 0);
+    //pointLight(255,255,255, 0, 0, 0);
     for (let i = 0; i < contador; i++){
       push();
       translate(eje_x_3[i], eje_y_3[i], eje_z_3[i]);
@@ -314,7 +314,7 @@ function draw(){
     //fill(250, 0, 300);
     texture(texturaPiedra4);
     noStroke();
-    pointLight(0,255,0, 0, 0, 0);
+    //pointLight(255,255,255, 0, 0, 0);
     for (let i = 0; i < contador; i++){
       push();
       translate(eje_x_4[i], eje_y_4[i], eje_z_4[i]);
